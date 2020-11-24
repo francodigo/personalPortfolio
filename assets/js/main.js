c2af0771dfa709686747ceb7e0f548fdfbb7af7a
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2020-11-24 16:28:08 -0300

    Se añade imagen About Me y Smooth scroll
    Agrego de la imagen en about me y su estilo
    CSS.
    Además agrego Jquery para realizar el Smooth Scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,12 @@ function scrollActive() {
     }
   });
 }
+
+$('.nav a ').on('click', function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr('href');
+  // 3
+  $('html, body').animate({ scrollTop: $(href).offset().top }, 800);
+});
