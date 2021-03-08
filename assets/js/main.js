b1f7fc98f053a5b609ddc7399b1958021f2b962b
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2021-03-08 16:01:20 -0300

    Se elimina la section Skills|Refactor|New project
    Se saca la sección Skilss. Se modifica el margen de
    las secciones porque colisionaban cuando era responsive.
    Se comenta mejor el código.
    Además se agregó el al portfolio un nuevo proyecto Incoming.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
 const navMenu = document.getElementById('nav-menu'),
-  toggleMenu = document.getElementById('nav-toggle'),
+  openMenu = document.getElementById('nav-open'),
   closeMenu = document.getElementById('nav-close');
 
-toggleMenu.addEventListener('click', () => {
+/** Open Menu */
+openMenu.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
-
+/** Close Menu */
 closeMenu.addEventListener('click', () => {
   navMenu.classList.remove('show');
 });
-
-/*Remove menu*/
 
 const navLink = document.querySelectorAll('.nav__link');
 
@@ -18,21 +17,22 @@ function linkAction() {
   navMenu.classList.remove('show');
 }
 
+/*Close Menu when click on the link (navLink)*/
+
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-/** Scroll sections */
-
+/** Active Links When your Scroll is over the section */
 const sections = document.querySelectorAll('section[id]');
 
 window.addEventListener('scroll', scrollActive);
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
-
   sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id');
+    const sectionHeight = current.offsetHeight; //height Element
+    const sectionTop = current.offsetTop - 50;  //Y-axis Top height from parent (Body) 
+    
+    sectionId = current.getAttribute('id'); 
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
